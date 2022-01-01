a30673d111a048dbd06b9fba0622bb8c345dc536
moduhan <devduhanmo@gmail.com>
2022-01-01 18:51:55 +0900

    first commit

---
+++ book/src/main/java/duhan/book/controller/GuestbookController.java
@@ -0,0 +1,18 @@
+package duhan.book.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/guestbook")
+public class GuestbookController {
+
+    @GetMapping({"", "/list"})
+    public String list() {
+        log.info("GetMapping / ");
+        return "/guestbook/list";
+    }
+}
---
+++ book/src/test/java/duhan/book/repository/GuestbookRepositoryTest.java
@@ -0,0 +1,27 @@
+package duhan.book.repository;
+
+import duhan.book.entity.Guestbook;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+class GuestbookRepositoryTest {
+
+    @Autowired
+    private GuestbookRepository guestbookRepository;
+
+    @Test
+    void insertDummies() {
+        IntStream.rangeClosed(1, 300).forEach(i -> {
+            Guestbook guestbook = Guestbook.builder()
+                    .title("Title..." + i)
+                    .content("Content..." + i)
+                    .writer("user" + (i % 10))
+                    .build();
+            System.out.println(guestbookRepository.save(guestbook));
+        });
+    }
+}
