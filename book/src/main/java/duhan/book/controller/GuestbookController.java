1591d28be973f7a329faf6d7f0afdeb3d9b30974
moduhan <devduhanmo@gmail.com>
2022-01-03 13:01:04 +0900

    feat: 수정,삭제,등록 페이징,검색 처리 구현

--- book/src/main/java/duhan/book/controller/GuestbookController.java
+++ book/src/main/java/duhan/book/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package duhan.book.controller;
 
+import duhan.book.dto.GuestbookDTO;
 import duhan.book.dto.PageRequestDTO;
 import duhan.book.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -27,5 +31,48 @@ public class GuestbookController {
     public void list(PageRequestDTO pageRequestDTO, Model model) {
         log.info("list()..........{}", pageRequestDTO);
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping({"/read", "/modify"})
+    public void read(long gno,
+                     @ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO,
+                     Model model) {
+        log.info("gno={}",gno);
+        model.addAttribute("dto", service.read(gno));
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        log.info("register get...");
+        return "/guestbook/register";
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto={}", dto);
+        Long gno = service.register(dto);
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/guestbook/list";
+    }
+
+    @PostMapping("/remove")
+    public String remove(long gno, RedirectAttributes redirectAttributes) {
+        log.info("gno={}", gno);
+        service.remove(gno);
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/guestbook/list";
+    }
+
+    @PostMapping("/modify")
+    public String modify(GuestbookDTO dto,
+                         @ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO,
+                         RedirectAttributes redirectAttributes) {
+        service.modify(dto);
+        redirectAttributes.addAttribute("page", pageRequestDTO.getPage());
+        redirectAttributes.addAttribute("type", pageRequestDTO.getType());
+        redirectAttributes.addAttribute("keyword", pageRequestDTO.getKeyword());
+        redirectAttributes.addAttribute("gno", dto.getGno());
+
+        return "redirect:/guestbook/read";
     }
 }
--- book/src/main/java/duhan/book/service/GuestbookServiceImpl.java
+++ book/src/main/java/duhan/book/service/GuestbookServiceImpl.java
@@ -1,9 +1,12 @@
 package duhan.book.service;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import duhan.book.dto.GuestbookDTO;
 import duhan.book.dto.PageRequestDTO;
 import duhan.book.dto.PageResultDTO;
 import duhan.book.entity.Guestbook;
+import duhan.book.entity.QGuestbook;
 import duhan.book.repository.GuestbookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -33,9 +37,61 @@ public class GuestbookServiceImpl implements GuestbookService {
     @Override
     public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
         Pageable pageable = requestDTO.getPageable(Sort.by("gno"));
-        Page<Guestbook> result = repository.findAll(pageable);
+        BooleanBuilder booleanBuilder = getSearch(requestDTO);
+        Page<Guestbook> result = repository.findAll(booleanBuilder, pageable);
+
         Function<Guestbook, GuestbookDTO> fn = (this::entityToDto);
         return new PageResultDTO<>(result, fn);
     }
 
+    @Override
+    public GuestbookDTO read(Long gno) {
+        Guestbook result = repository.findById(gno)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다"));
+       return entityToDto(result);
+    }
+
+    @Override
+    public void remove(Long gno) {
+        repository.deleteById(gno);
+    }
+
+    @Override
+    public void modify(GuestbookDTO dto) {
+        Guestbook entity = repository.findById(dto.getGno())
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다"));
+        entity.changeTitle(dto.getTitle());
+        entity.changeContent(dto.getContent());
+        repository.save(entity);
+    }
+
+    private BooleanBuilder getSearch(PageRequestDTO requestDTO) {
+        String type = requestDTO.getType();
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+        String keyword = requestDTO.getKeyword();
+        BooleanExpression expression = qGuestbook.gno.gt(0L);
+        booleanBuilder.and(expression);
+
+        if (type == null || type.trim().length() == 0) {
+            return booleanBuilder;
+        }
+
+        // 검색 조건을 작성
+        BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+        if (type.contains("t")) {
+            conditionBuilder.or(qGuestbook.title.contains(keyword));
+        }
+        if (type.contains("c")) {
+            conditionBuilder.or(qGuestbook.content.contains(keyword));
+        }
+        if (type.contains("w")) {
+            conditionBuilder.or(qGuestbook.writer.contains(keyword));
+        }
+        // 모든 조건 통합
+        booleanBuilder.and(conditionBuilder);
+
+        return booleanBuilder;
+    }
 }
